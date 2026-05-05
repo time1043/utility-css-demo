88465ce1ea694c8c240c9f4c48acc45c56301e29
oswin902m <1665434994@qq.com>
2026-05-05 20:28:26 +0800

    tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div>App</div>;
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500 text-center">
+      Hello world!
+    </h1>
+  );
 }
